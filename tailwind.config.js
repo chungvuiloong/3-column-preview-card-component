59a292801d99c3497aa4d9bc04aa250243d8fdbb
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-13 19:08:49 +0300

    Added custome colors.

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+        color: {
+            // Primary
+            'Bright-orange': 'hsl(31, 77%, 52%)',
+            'Dark-cyan': 'hsl(184, 100%, 22%)',
+            'Very-dark-cyan': 'hsl(179, 100%, 13%)',
+
+            // Neutral
+            'Transparent-white': 'hsla(0, 0%, 100%, 0.75)',
+            'Very-light-gray': 'hsl(0, 0%, 95%)'
+        }
+        },
+    },
+    plugins: [],
+}
