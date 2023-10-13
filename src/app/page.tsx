5ad5ba6b10b2db7bf41312b76e05e65a8d67eb81
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-13 21:22:19 +0300

    Added gap. Added text color.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
         <div className='h-screen container mx-auto flex flex-col justify-center'>        
             <div className='flex lg:flex-row flex-col rounded-lg overflow-hidden'>
                 <div className='bg-Bright-orange p-12 flex flex-col'>
-                    <div>
+                    <div className='flex flex-col gap-6'>
                         <Image src={Sedan_image} alt='Sedan Image' />
-                        <div >Sedans</div>
-                        <div>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</div>
+                        <div>Sedans</div>
+                        <div className='text-Transparent-white'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</div>
                     </div>
                     <button className='w-fit px-8 py-3 bg-white rounded-full text-Bright-orange'>Learn More</button>
                 </div> 
