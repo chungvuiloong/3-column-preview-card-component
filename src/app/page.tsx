615cadb0e56ea4adf1d04dac2bb3624f27adb51f
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-13 19:34:11 +0300

    Added text from starter files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,20 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-        Test
+        <div className='flex flex-row'>
+            <div>
+                <div>Sedans</div>
+                <div>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</div>
+            </div>    
+            <div>
+                <div>SUVs</div>
+                <div>Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</div>
+            </div>
+            <div>
+                <div>Luxury</div>
+                <div>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</div>
+            </div>
+        </div>
     </main>
   )
 }
