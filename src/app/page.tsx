b943ff24a48111a353a45f4b38b72263c04c64eb
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-13 20:42:18 +0300

    Add second col bg. Added image to first col.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Sedan_image from '@/assets/images/icon-sedans.svg'
 
 export default function Home() {
   return (
@@ -7,12 +8,13 @@ export default function Home() {
             <div className='flex lg:flex-row flex-col rounded-lg overflow-hidden'>
                 <div className='bg-Bright-orange p-12 flex flex-col'>
                     <div>
+                        <Image src={Sedan_image} alt='Sedan Image' />
                         <div >Sedans</div>
                         <div>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</div>
                     </div>
-                    <button>Learn More</button>
+                    <button className='w-fit px-8 py-3 bg-white rounded-full text-Bright-orange'>Learn More</button>
                 </div> 
-                <div className='bg-Bright-orange p-12'>
+                <div className='bg-Dark-cyan p-12'>
                     <div>
                         <div>SUVs</div>
                         <div>Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</div>
